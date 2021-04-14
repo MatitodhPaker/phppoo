ebf94eab733fc34340997861ad3c64ce5c38bc61
MatitodhPaker <hueco35.perez@gmail.com>
2021-04-14 07:34:25 -0500

    nueva tarea

---
+++ clases.php
@@ -0,0 +1,12 @@
+<?php
+  //una clase es nuestro molde de trabajo 
+  //para nuestros metodos y atributos
+  class miClase{
+    // definir los atributos 
+
+    //definir los metodos
+  }
+  class Miclase2{
+    
+  }
+?>
