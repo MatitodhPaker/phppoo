31e1ab8d53600ef18e6df4d5fe9f5c5ff27bcedd
MatitodhPaker <hueco35.perez@gmail.com>
2021-04-14 13:36:25 -0500

    se modifico el index y se agrego claseCalculo.php y procesar.php

--- calculadora/index.php
+++ calculadora/index.php
@@ -4,8 +4,23 @@
   <title>Calculadora</title>
 </head>
 <body>
-  <form action="calcular.php" method="post">
-    <label for=""></label>
+  <form action="procesar.php" method="post">
+    <label>valor1</label>
+    <p></p>
+    <input type="text" name="val1">
+    <p></p>
+    <label >valor2</label>
+    <p></p>
+    <input type="text" name="val2">
+    <p></p>
+    <select name="opcion">
+      <option value="suma">Suma</option>
+      <option value="resta">Resta</option>
+      <option value="division">Division</option>
+      <option value="multiplicacion">Multiplicacion</option>
+    </select>
+    <p></p>
+    <button>calcular</button>
   </form>
 </body>
 </html>
