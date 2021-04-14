f8c6f1a04fe0c6ca546c56bc1e979a3599348647
MatitodhPaker <hueco35.perez@gmail.com>
2021-04-14 11:02:40 -0500

    se agrego la carpeta de calculadora

---
+++ calculadora/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Calculadora</title>
+</head>
+<body>
+  <form action="calcular.php" method="post">
+    <label for=""></label>
+  </form>
+</body>
+</html>
